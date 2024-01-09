b08bafd5f26231f634fe2d6975508ae4691f0fa0
wsvaio <wsvaio@qq.com>
2024-01-09 14:26:48 +0800

    fix: exec现在更加合理（会动态移除ctx.error若有），小程序context3问题，ntiveFetch对get与head方法body的处理

--- src/context.ts
+++ src/context.ts
@@ -1,27 +1,30 @@
 import type { AfterPatch, BasicContext, BeforeContext, BeforePatch, Requester } from "./types";
 import { mergeContext } from "./utils";
 
+export const CTX = {
+  method: "get",
+  headers: {},
+  log: false,
+
+  path: "/",
+  base: "",
+
+  query: {},
+  body: null,
+  param: {},
+
+  befores: [],
+  afters: [],
+  errors: [],
+  finals: [],
+};
+
 export function createContext<T extends Record<any, any>, B extends BeforePatch, A extends AfterPatch>(
   initial: Partial<Omit<BasicContext<B, A>, "requester">> & { requester: typeof Requester<B, A> } & T
 ) {
-  return mergeContext(
-    {
-      method: "get",
-      headers: {},
-      log: false,
+  return mergeContext({}, CTX, initial) as BeforeContext<B & T, A>;
+}
 
-      path: "/",
-      base: "",
-
-      query: {},
-      body: null,
-      param: {},
-
-      befores: [],
-      afters: [],
-      errors: [],
-      finals: [],
-    },
-    initial
-  ) as BeforeContext<B & T, A>;
+export function setGlobalContext(ctx: Partial<Omit<BasicContext, "requester">>) {
+  mergeContext(CTX, ctx);
 }
